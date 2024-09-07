f7af9d827e1ee1fa128f748ac2fad8a72cd94f4a
Madhukar Palakaveeti <madhupalakaveeti@gmail.com>
2024-09-08 01:28:02 +0530

    Added dataset

---
+++ dataset_extraction.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import os
+import json
+import numpy as np
+
+data = []
+access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
+HEADERS = {'accept' : 'application/vnd.github+json', 
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': f'Bearer {access_token}'
+          }
+with open('user_urls.json', 'r') as f:
+    ob = json.load(f)
+
+full_urls = ob['urls']
+full_urls = full_urls[:5000]
+urls = np.random.choice(full_urls, size=5000, replace=False)
+for url in urls:
+    user_url = url['user_url']
+    repos_url = url['repos_url']
+    user_res = requests.get(headers=HEADERS,url=user_url)
+    repos_res = requests.get(headers=HEADERS,url=repos_url)
+    if user_res.status_code == 200 and repos_res.status_code == 200:
+        batch_data = {}
+        user_data = user_res.json()
+        repos_data = repos_res.json()
+        batch_data['login'] = user_data['login']
+        batch_data['user_url'] = user_data['url']
+        batch_data['html_url'] =  user_data['html_url']
+        batch_data['repos_url'] =  user_data['repos_url']
+        batch_data['followers'] = user_data['followers']
+        batch_data['repo_details'] = [{'language' : repo['language'], 
+                                       'stars' : repo['stargazers_count'],
+                                       'forks' : repo['forks'],
+                                       'watchers' : repo['watchers']}
+                                     for repo in repos_data ]
+        data.append(batch_data)
+    time.sleep(2)
+
+with open('dataset.json' , 'w') as f:
+    json.dump({'data' : data}, f)
--- git_requests.py
+++ git_requests.py
@@ -2,33 +2,35 @@ import requests
 import time 
 import json
 import os
-
+import random
 
 access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
-with open('repo_urls.json', 'r') as f:
-    ob = json.load(f)
-urls = ob['urls']
+HEADERS = {'accept' : 'application/vnd.github+json', 
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': f'Bearer {access_token}'
+            }
 
-repo_num = 6001
-while repo_num < 500000:
-    main_url = f'https://api.github.com/repositories?since={repo_num}'
-    response = requests.get(headers={'accept' : 'application/vnd.github+json', 
-                                 'X-GitHub-Api-Version': '2022-11-28',
-                                 'Authorization': f'Bearer {access_token}'
-                                 }, 
-                        url=main_url)
+urls = []
+
+user_id = random.choice(range(10000))
+max_id = user_id + 20000
+while user_id < max_id:
+    main_url = f'https://api.github.com/users?since={user_id}'
+    response = requests.get(headers=HEADERS, url=main_url)
     if response.status_code == 200:
         batch_urls = []
-        repos = response.json()
-        for repo in repos:
-            batch_urls.append(repo.get('url'))
+        users = response.json()
+        for user in users:
+            user_url = user.get('url')
+            repos_url = user.get('repos_url')
+            batch_urls.append({'user_url' : user_url, 'repos_url' : repos_url})
         urls.extend(batch_urls)
-        last_id = repos[-1].get('id')
-    repo_num = last_id + 1
+        last_id = users[-1].get('id')
+    user_id = last_id + 1
 
-    time.sleep(1.5)
+    time.sleep(0.5)
    
-with open('new_repo_urls.json', 'w') as f:
+with open('user_urls.json', 'w') as f:
     json.dump({'urls' : urls, 'last_id' : last_id}, f)
 
 
