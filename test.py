aa7a7c10227f1f87366d03fc3fec9804b74e99c1
Madhukar Palakaveeti <madhupalakaveeti@gmail.com>
2024-09-09 22:43:18 +0530

    Trained a small model

--- test.py
+++ test.py
@@ -11,24 +11,36 @@ HEADERS = {'accept' : 'application/vnd.github+json',
             'Authorization': f'Bearer {access_token}'
             }
 # # url = 'https://api.github.com/users'
-user_res = requests.get(headers=HEADERS,url='https://api.github.com/users/asyraf9')
-repos_res = requests.get(headers=HEADERS,url='https://api.github.com/users/asyraf9/repos')
-# # if user_res.status_code == 200 and repos_res.status_code == 200:
-# #     batch_data = {}
-# #     user_data = user_res.json()
-# #     repos_data = repos_res.json()
-# #     batch_data['login'] = user_data['login']
-# #     batch_data['user_url'] = user_data['url']
-# #     batch_data['html_url'] =  user_data['html_url']
-# #     batch_data['repos_url'] =  user_data['repos_url']
-# #     batch_data['followers'] = user_data['followers']
-# #     batch_data['repo_details'] = [{'language' : repo['language'], 
-# #                                     'stars' : repo['stargazers_count'],
-# #                                     'forks' : repo['forks'],
-# #                                     'watchers' : repo['watchers']}
-# #                                     for repo in repos_data ]
+user_res = requests.get(headers=HEADERS,url='https://api.github.com/users/yacineMTB')
+repos_res = requests.get(headers=HEADERS,url='https://api.github.com/users/yacineMTB/repos')
+if user_res.status_code == 200 and repos_res.status_code == 200:
+    batch_data = {}
+    user_data = user_res.json()
+    repos_data = repos_res.json()
+    batch_data['login'] = user_data['login']
+    batch_data['user_url'] = user_data['url']
+    batch_data['html_url'] =  user_data['html_url']
+    batch_data['repos_url'] =  user_data['repos_url']
+    batch_data['followers'] = user_data['followers']
+    batch_data['languages'] = set()
+    batch_data['total_stars'] = 0
+    batch_data['total_forks'] = 0
+    batch_data['total_watchers'] = 0
+    batch_data['repo_details'] = [{'language' : repo['language'], 
+                                    'stars' : repo['stargazers_count'],
+                                    'forks' : repo['forks'],
+                                    'watchers' : repo['watchers']}
+                                    for repo in repos_data ]
     
-print(repos_res.headers)
+    for repo in batch_data['repo_details']:
+        if repo['language'] is not None:
+            batch_data['languages'].add(repo['language'])
+        batch_data['total_stars'] += repo['stars']
+        batch_data['total_forks'] += repo['forks']
+        batch_data['total_watchers'] += repo['watchers']
+        batch_data['repo_count'] = len(batch_data['repo_details'])
+    
+print([batch_data['followers'], batch_data['total_stars'], batch_data['total_forks'], batch_data['total_watchers'], batch_data['repo_count']])
 # with open('resources/dataset.json', 'r') as f:
 #     d = json.load(f)
 # data = d['data']
